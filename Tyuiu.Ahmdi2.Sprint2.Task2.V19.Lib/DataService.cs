bea1d4a5d2d006a1751be578b596b2d544aeec66
Ahmadi1 <Ahmadi@DS10BK3>
2025-10-28 08:21:11 -0700

    task2

--- Tyuiu.Ahmdi2.Sprint2.Task2.V19.Lib/DataService.cs
+++ Tyuiu.Ahmdi2.Sprint2.Task2.V19.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.Ahmadi2.Sprint2.Task2.V19.Lib
     {
         public bool CheckDotInShadedArea(int x, int y)
         {
-            // منطقه سایه‌دار: مربع از (5,5) تا (11,11)
+            //  (5,5)  (11,11)
             bool inShadedArea = (x >= 5 && x <= 11) && (y >= 5 && y <= 11);
 
             return inShadedArea;
