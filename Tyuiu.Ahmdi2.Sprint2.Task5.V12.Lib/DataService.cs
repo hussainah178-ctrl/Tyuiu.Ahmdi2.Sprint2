a3c6696b7560e3e4044e7b98c2004efa7af10be3
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-03 23:02:12 -0800

    task5

---
+++ Tyuiu.Ahmdi2.Sprint2.Task5.V12.Lib/DataService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Tyuiu.Ahmadi2.Sprint2.Task5.V12.Lib
+{
+    public class DataService
+    {
+        public string FindDateOfPreviousDay(int g, int m, int n)
+        {
+            int prevDay = n - 1;
+            int prevMonth = m;
+            int prevYear = g;
+
+            if (prevDay == 0)
+            {
+                prevMonth--;
+
+                if (prevMonth == 0)
+                {
+                    prevMonth = 12;
+                    prevYear--;
+                }
+
+                switch (prevMonth)
+                {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        prevDay = 31;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        prevDay = 30;
+                        break;
+                    case 2:
+                        prevDay = 29; 
+                        break;
+                }
+            }
+
+            return $"{prevDay}.{prevMonth}.{prevYear}";
+        }
+    }
+}
